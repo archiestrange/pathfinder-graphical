583492efee4a0a0cd4cf0099fedb535c0e283e02
Archie Strange <archiestrange@gmail.com>
2019-07-24 20:22:07 +0100

    Handle window resize with SVG lines

--- src/components/pathfinder.tsx
+++ src/components/pathfinder.tsx
@@ -30,28 +30,45 @@ export class Pathfinder extends React.Component<ComponentProps, LocalState> {
     this.calculate = this.calculate.bind(this);
     this.updateInputA = this.updateInputA.bind(this);
     this.updateInputB = this.updateInputB.bind(this);
+    this.handleWindowResize = this.handleWindowResize.bind(this);
   }
   
+  handleWindowResize() {
+    if(this.state.result) {
+      this.reset();
+    }
+  }
+
+  componentWillMount(){
+    // Stop fixed svg lines moving out of place on window resize
+    window.addEventListener("resize", this.handleWindowResize);
+  }
+
   componentDidUpdate() {
+    // Automatically calculate results when the second orb is selected
     if (this.state.siteA && this.state.siteB && !this.state.result) {
         this.calculate();
     }
   }
 
+  // Handle selection of first orb
   updateInputA(siteA: Destination): void {
     this.setState({ siteA });
   }
   
+  // Handle selection of second orb
   updateInputB(siteB: Destination): void {
     this.setState({ siteB });
   }
 
+  // Calculate the shortest path between the two selected routes
   calculate(): void {
     const { siteA, siteB } = this.state;
     const result = Calculate(siteA!, siteB!);
     this.setState({ result: convertResultToStringArray(result), bestResult: result[0] });
   }
 
+  // Set state back to original state
   reset() {
     this.setState(initialState);
   }
